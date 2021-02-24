3911337aaf245d59df084a0f0275c98a89285955
Javvvve <1099046808@qq.com>
2021-02-24 22:13:57 +0800

    feat(login): 完成登录框样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="mu-login">
     <v-card>
-        <v-card-title class="headline lighten-2">
+      <v-icon class="mu-login-icon-close" @click="handleClose">close</v-icon>
+      <v-card-title class="headline lighten-2">
+        <span class="mu-login-title">登录</span>
+      </v-card-title>
+      <v-card-text class="mu-login-content">
+        <v-text-field
+          label="手机号"
+          class="mu-login-input"
+          prepend-icon="mdi-account"
+          hide-details="auto"
+        ></v-text-field>
+        <v-text-field
+          label="验证码"
+          class="mu-login-input"
+          prepend-icon="mdi-lock"
+          hide-details="auto"
+        >
+          <template v-slot:append>
+            <span class="mu-login-code">获取验证码</span>
+          </template>
+        </v-text-field>
+        <p class="mu-login-text">其他登录方式</p>
+        <v-icon class="mu-login-icon-wechat">mdi-wechat</v-icon>
+      </v-card-text>
+      <p class="mu-login-description">登录/注册即代表同意《用户协议》</p>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click="dialog = false"
+        >
           登录
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -32,10 +48,61 @@ export default {
     return {
       
     }
+  },
+  methods: {
+    handleClose() {
+      this.$emit('close');
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+$r: mu-login;
+
+.#{$r} {
+  text-align: center;
+  &-title {
+    font-size: 18px;
+  }
+  &-content {
+    min-height: 300px;
+  }
+  &-input {
+    margin-top: 30px;
+  }
+  &-code {
+    font-size: 12px;
+    color: #1976d2;
+    cursor: pointer;
+    margin-right: 5px;
+    &:hover {
+      opacity: .8;
+    }
+  }
+  &-icon {
+    &-close {
+      position: absolute;
+      top: 16px;
+      right: 10px;
+    }
+    &-wechat {
+      font-size: 40px;
+      cursor: pointer;
+      color: green;
+    }
+  }
+  &-text {
+    font-size: 14px;
+    margin-top: 30px;
+    text-align: center;
+    opacity: .8;
+  }
+  &-description {
+    font-size: 12px;
+    text-align: center;
+    opacity: .6;
+  }
+}
 
 </style>
